6b52402767690479045b8f2970ff51d3b16afc30
franck <carnet.franck.paul@gmail.com>
2013-09-19 13:27:27 +0200

    various

--- _define.php
+++ _define.php
@@ -1,14 +1,15 @@
 <?php
 # -- BEGIN LICENSE BLOCK ----------------------------------
+# This file is part of googleTools, a plugin for Dotclear 2.
 #
-# This file is part of Dotclear 2.
+# Copyright (c) Olivier Meunier, Franck Paul and contributors
 #
-# Copyright (c) 2003-2013 Olivier Meunier, Franck Paul and contributors
 # Licensed under the GPL version 2.0 license.
-# See LICENSE file or
+# A copy of this license is available in LICENSE file or at
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-#
 # -- END LICENSE BLOCK ------------------------------------
+
+if (!defined('DC_RC_PATH')) { return; }
 
 $this->registerModule(
 	/* Name */			"Markdown syntax",
