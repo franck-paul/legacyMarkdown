21ba1cc5d6286def66c64ff31b9ea310d7e47a72
franck <carnet.franck.paul@gmail.com>
2022-12-26 18:20:07 +0100

    New install

--- _install.php
+++ _install.php
@@ -14,26 +14,23 @@ if (!defined('DC_CONTEXT_ADMIN')) {
     return;
 }
 
-$new_version = dcCore::app()->plugins->moduleInfo('formatting-markdown', 'version');
-$old_version = dcCore::app()->getVersion('formatting-markdown');
-
-if (version_compare((string) $old_version, $new_version, '>=')) {
+if (!dcCore::app()->newVersion(basename(__DIR__), dcCore::app()->plugins->moduleInfo(basename(__DIR__), 'version'))) {
     return;
 }
 
 try {
+    $old_version = dcCore::app()->getVersion(basename(__DIR__));
+
     if (version_compare((string) $old_version, '1.10', '<')) {
         # A bit of housecleaning for no longer needed files
         $remfiles = [
-            'inc/markdown.php',
-            'inc/License.text',
+            ['inc','markdown.php'],
+            ['inc','License.text'],
         ];
         foreach ($remfiles as $f) {
-            @unlink(DC_ROOT . '/' . $f);
+            @unlink(dcUtils::path([__DIR__, ...$f]));
         }
     }
-
-    dcCore::app()->setVersion('formatting-markdown', $new_version);
 
     return true;
 } catch (Exception $e) {
