d2b12d6f9638a3c5e05fa25e57dee42de69471ae
franck <carnet.franck.paul@gmail.com>
2024-06-09 15:36:34 +0200

    New release 5.4.1

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'Markdown syntax',
     'Brings you markdown syntax for your entries',
     'Michel Fortin, Franck Paul and contributors',
-    '5.4',
+    '5.4.1',
     [
         'requires'    => [['core', '2.30']],
         'permissions' => 'My',
