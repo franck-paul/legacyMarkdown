a948e46091631d0a986e74ed92f538268b529a44
franck <carnet.franck.paul@gmail.com>
2025-08-29 11:02:03 +0200

    New release 8.6

--- _define.php
+++ _define.php
@@ -15,9 +15,9 @@ $this->registerModule(
     'Markdown syntax',
     'Brings you markdown syntax for your entries',
     'Michel Fortin, Franck Paul and contributors',
-    '8.5',
+    '8.6',
     [
-        'date'     => '2025-08-25T11:19:46+0200',
+        'date'     => '2025-08-29T11:02:03+0200',
         'requires' => [
             ['core', '2.35'],
             ['TemplateHelper'],
