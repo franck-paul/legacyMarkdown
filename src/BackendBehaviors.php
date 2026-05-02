c92b8569ac6344b6c24aebcbc90a991360055fba
franck <contact@open-time.net>
2026-05-02 10:37:26 +0200

    Rector coding style

--- src/BackendBehaviors.php
+++ src/BackendBehaviors.php
@@ -149,9 +149,11 @@ class BackendBehaviors
             if ($excerpt !== '') {
                 $params['excerpt'] = Helper::fromHTML($excerpt);
             }
+
             if ($content !== '') {
                 $params['content'] = Helper::fromHTML($content);
             }
+
             $params['format'] = 'markdown';
 
             return __('Don\'t forget to validate your Markdown conversion by saving your post.');
@@ -385,6 +387,7 @@ class BackendBehaviors
         if (isset($cols['posts']) && is_array($cols['posts']) && is_array($cols['posts'][1])) {
             $cols['posts'][1] = array_merge($cols['posts'][1], $template);
         }
+
         if (isset($cols['pages']) && is_array($cols['pages']) && is_array($cols['pages'][1])) {
             $cols['pages'][1] = array_merge($cols['pages'][1], $template);
         }
