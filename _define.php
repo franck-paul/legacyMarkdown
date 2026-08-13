3f2ca795f326d23bde187a198b9036406c8255fb
franck <contact@open-time.net>
2026-08-13 16:42:28 +0200

    Enforces some verification before registering module

--- _define.php
+++ _define.php
@@ -11,27 +11,31 @@
  * @copyright Franck Paul contact@open-time.net
  * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
  */
-$this->registerModule(
-    'Markdown syntax',
-    'Brings you markdown syntax for your entries',
-    'Michel Fortin, Franck Paul and contributors',
-    '11.0',
-    [
-        'date'     => '2026-08-03T10:02:32+0200',
-        'requires' => [
-            ['core', '2.39'],
-            ['TemplateHelper'],
-        ],
-        'permissions' => 'My',
-        'type'        => 'plugin',
-        'priority'    => 1005,  // Must be higher than dcLegacyEditor/dcCKEditor priority (ie 1000)
-        'settings'    => [
-            'pref' => '#user-options.user_options_edition',
-            'blog' => '#params.legacy_markdown',
-        ],
-        'details'    => 'https://open-time.net/?q=legacyMarkdown',
-        'support'    => 'https://github.com/franck-paul/legacyMarkdown',
-        'repository' => 'https://raw.githubusercontent.com/franck-paul/legacyMarkdown/main/dcstore.xml',
-        'license'    => 'gpl2',
-    ]
-);
+declare(strict_types=1);
+
+if (isset($this) && is_object($this) && method_exists($this, 'registerModule') && isset($this->id) && is_string($this->id)) {
+    $this->registerModule(
+        'Markdown syntax',
+        'Brings you markdown syntax for your entries',
+        'Michel Fortin, Franck Paul and contributors',
+        '11.0',
+        [
+            'date'     => '2026-08-03T10:02:32+0200',
+            'requires' => [
+                ['core', '2.39'],
+                ['TemplateHelper'],
+            ],
+            'permissions' => 'My',
+            'type'        => 'plugin',
+            'priority'    => 1005,  // Must be higher than dcLegacyEditor/dcCKEditor priority (ie 1000)
+            'settings'    => [
+                'pref' => '#user-options.user_options_edition',
+                'blog' => '#params.legacy_markdown',
+            ],
+            'details'    => 'https://open-time.net/?q=legacyMarkdown',
+            'support'    => 'https://github.com/franck-paul/legacyMarkdown',
+            'repository' => 'https://raw.githubusercontent.com/franck-paul/legacyMarkdown/main/dcstore.xml',
+            'license'    => 'gpl2',
+        ]
+    );
+}
